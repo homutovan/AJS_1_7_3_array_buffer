975cf0f3a72ae19727453b2473c1e0baa405b504
Andrey Khomutov <homutovan@live.com>
2020-09-02 00:46:15 +0700

    fix eslint errors

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,20 @@
 export function getBuffer(data) {
-    return (input => {
-      const buffer = new ArrayBuffer(data.length * 2);
-      const bufferView = new Uint16Array(buffer);
-      for (let i = 0; i < input.length; i++) {
-        bufferView[i] = input.charCodeAt(i);
-      }
-      return buffer;
-    })(data);
+  return ((input) => {
+    const buffer = new ArrayBuffer(data.length * 2);
+    const bufferView = new Uint16Array(buffer);
+    for (let i = 0; i < input.length; i++) {
+      bufferView[i] = input.charCodeAt(i);
+    }
+    return buffer;
+  })(data);
+}
+
+export default class ArrayBufferConverter {
+  static load(buffer) {
+    this.buffer = buffer;
   }
 
-  export default class ArrayBufferConverter {
-    static load(buffer) {
-      this.buffer = buffer;
-    }
-  
-    static toString() {
-      return [...new Uint16Array(this.buffer)].map(e => String.fromCharCode(e)).join('');
-    }
+  static toString() {
+    return [...new Uint16Array(this.buffer)].map(e => String.fromCharCode(e)).join('');
   }
+}
